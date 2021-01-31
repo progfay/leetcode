db5d98b4ae1ac2232754cb4bdc94a406ea4a101c
progfay <shun1856shun@gmail.com>
2021-01-31 17:22:19 +0900

    feat(cpp): Next Permutation

---
+++ next-permutation/solution.cpp
@@ -0,0 +1,13 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution
+{
+public:
+  void nextPermutation(vector<int> &nums)
+  {
+    next_permutation(nums.begin(), nums.end());
+  }
+};
