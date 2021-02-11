af6d6660030ed987abf1ad4916a4348f877e3ddd
progfay <shun1856shun@gmail.com>
2021-02-11 18:38:57 +0900

    feat(cpp): Valid Anagram

---
+++ valid-anagram/solution.cpp
@@ -0,0 +1,29 @@
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+class Solution
+{
+public:
+  bool isAnagram(string s, string t)
+  {
+    if (s.length() != t.length())
+    {
+      return false;
+    }
+
+    sort(s.begin(), s.end());
+    sort(t.begin(), t.end());
+
+    for (int i = 0; i < s.length(); i++)
+    {
+      if (s[i] != t[i])
+      {
+        return false;
+      }
+    }
+
+    return true;
+  }
+};
