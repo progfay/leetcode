e63fb2ef6e131849139587a7a82f7d37dcf350a6
progfay <shun1856shun@gmail.com>
2021-03-01 23:56:29 +0900

    refactor(cpp): Distribute Candies

--- distribute-candies/solution.cpp
+++ distribute-candies/solution.cpp
@@ -1,11 +1,23 @@
 #include <vector>
-#include <set>
+#include <bitset>
+#include <iostream>
 
 using namespace std;
 
 class Solution {
 public:
     int distributeCandies(vector<int>& candyType) {
-      return min(set<int>(candyType.begin(), candyType.end()).size(), candyType.size() / 2);
+      ios_base::sync_with_stdio(0), cin.tie(0);
+
+      int limit = candyType.size() >> 1;
+      bitset<200001> bits(0);
+      for (int type : candyType)
+      {
+        bits.set(type + 1e5);
+      }
+
+      int count = bits.count();
+
+      return count > limit ? limit : count;
     }
 };
