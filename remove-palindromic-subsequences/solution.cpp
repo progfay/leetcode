8e33d67b26cdda11d10f615b474cbd726d3a0b53
progfay <shun1856shun@gmail.com>
2021-03-08 22:31:32 +0900

    feat(cpp): Remove Palindromic Subsequences

---
+++ remove-palindromic-subsequences/solution.cpp
@@ -0,0 +1,22 @@
+#include <string>
+
+using namespace std;
+
+class Solution
+{
+public:
+  int removePalindromeSub(string s)
+  {
+    if (s.length() == 0)
+      return 0;
+
+    int left = 0, right = s.length() - 1;
+    while (left < right)
+    {
+      if (s[left++] != s[right--])
+        return 2;
+    }
+
+    return 1;
+  }
+};
