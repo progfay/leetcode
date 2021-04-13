e27814768c9958d7956a280d51298c64e26780e9
progfay <shun1856shun@gmail.com>
2021-04-13 22:31:56 +0900

    feat(cpp): Flatten Nested List Iterator

---
+++ flatten-nested-list-iterator/solution.cpp
@@ -0,0 +1,75 @@
+#include <vector>
+
+using namespace std;
+
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * class NestedInteger {
+ *   public:
+ *     // Return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     bool isInteger() const;
+ *
+ *     // Return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // The result is undefined if this NestedInteger holds a nested list
+ *     int getInteger() const;
+ *
+ *     // Return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // The result is undefined if this NestedInteger holds a single integer
+ *     const vector<NestedInteger> &getList() const;
+ * };
+ */
+
+class NestedInteger
+{
+public:
+  // Return true if this NestedInteger holds a single integer, rather than a nested list.
+  bool isInteger() const;
+
+  // Return the single integer that this NestedInteger holds, if it holds a single integer
+  // The result is undefined if this NestedInteger holds a nested list
+  int getInteger() const;
+
+  // Return the nested list that this NestedInteger holds, if it holds a nested list
+  // The result is undefined if this NestedInteger holds a single integer
+  const vector<NestedInteger> &getList() const;
+};
+
+class NestedIterator
+{
+public:
+  NestedIterator(vector<NestedInteger> &nestedList)
+  {
+    for (auto x : nestedList)
+      flatten(x);
+  }
+
+  int next()
+  {
+    return flatList[cursor++];
+  }
+
+  bool hasNext()
+  {
+    return cursor < flatList.size();
+  }
+
+private:
+  void flatten(NestedInteger x)
+  {
+    if (x.isInteger())
+      flatList.push_back(x.getInteger());
+    else
+      for (auto y : x.getList())
+        flatten(y);
+  }
+
+  vector<int> flatList;
+  int cursor = 0;
+};
+
+/**
+ * Your NestedIterator object will be instantiated and called as such:
+ * NestedIterator i(nestedList);
+ * while (i.hasNext()) cout << i.next();
+ */
