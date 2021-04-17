11062eeee842cf65120eb46e67f1c9a7ea7d410a
progfay <shun1856shun@gmail.com>
2021-04-17 18:56:54 +0900

    feat(cpp): Number of Submatrices That Sum to Target

---
+++ number-of-submatrices-that-sum-to-target/solution.cpp
@@ -0,0 +1,38 @@
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+  int numSubmatrixSumTarget(vector<vector<int>> &matrix, int target)
+  {
+    int n = matrix.size();
+    int m = matrix.front().size();
+
+    for (int i = 0; i < n; i++)
+      for (int j = 1; j < m; j++)
+        matrix[i][j] += matrix[i][j - 1];
+
+    int ans = 0;
+    map<int, int> count_map;
+
+    for (int i = 0; i < m; i++)
+    {
+      for (int j = i; j < m; j++)
+      {
+        count_map.clear();
+        count_map[0] = 1;
+        int sum = 0;
+        for (int k = 0; k < n; k++)
+        {
+          sum += matrix[k][j] - (i > 0 ? matrix[k][i - 1] : 0);
+          ans += count_map[sum - target];
+          count_map[sum]++;
+        }
+      }
+    }
+
+    return ans;
+  }
+};
