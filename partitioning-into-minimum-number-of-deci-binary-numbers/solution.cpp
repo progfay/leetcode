a517b45fd9a51b212cda336ab685b81595922393
progfay <shun1856shun@gmail.com>
2021-05-26 22:30:18 +0900

    feat(cpp): Partitioning Into Minimum Number Of Deci-Binary Numbers

---
+++ partitioning-into-minimum-number-of-deci-binary-numbers/solution.cpp
@@ -0,0 +1,15 @@
+#include <string>
+
+using namespace std;
+
+class Solution
+{
+public:
+  int minPartitions(string n)
+  {
+    int ans = 0;
+    for (auto c : n)
+      ans = max(ans, c - '0');
+    return ans;
+  }
+};
