a0cb8e6e6b2e3a3cf80c5f55b69081ec811c2a2d
progfay <shun1856shun@gmail.com>
2021-05-31 22:39:45 +0900

    feat(cpp): Maximum Gap

---
+++ maximum-gap/solution.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution
+{
+public:
+  int maximumGap(vector<int> &nums)
+  {
+    sort(nums.begin(), nums.end());
+    int ans = 0;
+    for (int i = 1; i < nums.size(); i++)
+      ans = max(ans, nums[i] - nums[i - 1]);
+
+    return ans;
+  }
+};
