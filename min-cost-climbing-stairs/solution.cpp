b3aa5efc9b5be3a700046f5763fe5b7e1ced8b85
progfay <shun1856shun@gmail.com>
2021-06-08 01:02:36 +0900

    feat(cpp): Min Cost Climbing Stairs

---
+++ min-cost-climbing-stairs/solution.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+  int minCostClimbingStairs(vector<int> &cost)
+  {
+    int size = cost.size();
+    vector<int> dp(size, 0);
+    dp[0] = cost[0];
+    dp[1] = cost[1];
+
+    for (int i = 2; i < size; i++)
+      dp[i] = min(dp[i - 1], dp[i - 2]) + cost[i];
+
+    return min(dp[size - 1], dp[size - 2]);
+  }
+};
